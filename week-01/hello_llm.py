fe332cd8abc785d1d6feb4677c5d08899017d5da
Shiv <shivkumar4883@gmail.com>
2026-06-20 20:28:14 +0530

    updated function to handle exceptions and also user type hints for functions

--- week-01/hello_llm.py
+++ week-01/hello_llm.py
@@ -6,28 +6,34 @@ load_dotenv()
 
 PROMPT = "Explain what a large language model is in 2 sentences."
 
-def call_gemini(prompt):
+def call_gemini(prompt: str) -> str:
     client = genai.Client()
-    response = client.models.generate_content(
-    model="gemini-3.5-flash",
-    contents=prompt
-    )
-    return response.text
-    pass
+    try:
+        response = client.models.generate_content(
+        model="gemini-3.5-flash",
+        contents=prompt 
+        )
+        return response.text
+    except Exception as e:
+        return f"Gemini Error: {e}"
+    
 
-def call_groq(prompt):
-    client = Groq()
-    chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": prompt,
-        }
-    ],
-    model="llama-3.3-70b-versatile",
-    )
-    return chat_completion.choices[0].message.content
-    pass
+def call_groq(prompt: str) -> str:
+    try:
+        client = Groq()
+        chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="llama-3.3-70b-versatile",
+        )
+        return chat_completion.choices[0].message.content
+    except Exception as e:
+        return f"groq Error: {e}"
+    
 
 if __name__ == "__main__":
     print("==Gemini==")
